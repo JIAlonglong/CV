9c08dd6f0dedc3a6dd5d508335b10239728e0a33
JIAlonglong <2495477531@qq.com>
2022-11-16 10:53:26 +0800

    11

--- lidar_learning/build/catkin_generated/setup_cached.sh
+++ lidar_learning/build/catkin_generated/setup_cached.sh
@@ -5,10 +5,12 @@
 # it emulates the modifications of the setup script without recurring computations
 
 # new environment variables
+export ROSLISP_PACKAGE_DIRECTORIES='/home/tianbot/github/ros/lidar_learning/devel/share/common-lisp'
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/tianbot/github/ros/lidar_learning/devel:$CMAKE_PREFIX_PATH"
 export LD_LIBRARY_PATH="/home/tianbot/github/ros/lidar_learning/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/tianbot/github/ros/lidar_learning/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/tianbot/github/ros/lidar_learning/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/tianbot/github/ros/lidar_learning/devel/share/common-lisp'
+export PYTHONPATH="/home/tianbot/github/ros/lidar_learning/devel/lib/python2.7/dist-packages:$PYTHONPATH"
 export ROS_PACKAGE_PATH="/home/tianbot/github/ros/lidar_learning/src:$ROS_PACKAGE_PATH"
